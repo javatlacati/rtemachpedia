b4902b02e7ebc14e76b160b817089cbad62d9c5b
Javatlacati <scherzo_16@hotmail.com>
2024-11-01 14:08:37 -0600

    se agrega libreria de iconos

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,12 +1,29 @@
-import React, { FC } from 'react';
+import {FC, useState} from 'react';
+import {Card} from "primereact/card";
+import {Toast} from "primereact/toast";
+import {InputText} from "primereact/inputtext";
 
 
-interface SearchProps {}
+interface SearchProps {
+}
 
-const Search: FC<SearchProps> = () => (
-  <div>
-    Search Component
-  </div>
-);
+const Search: FC<SearchProps> = () => {
+
+  const [searchQuery, setSearchQuery] = useState('');
+
+  return (
+    <Card>
+      <Toast></Toast>
+      <div className="grid grid-cols-1 md:grid-cols-12">
+        <span className="p-input-icon-left md:col-span-11 md:pr-2">
+      <i className="pi pi-search"></i>
+        <InputText value={searchQuery}
+                   onChange={(e: React.ChangeEvent<HTMLInputElement>) => setSearchQuery(e.target.value)} minLength={5}
+                   maxLength={35}/>
+        </span>
+      </div>
+    </Card>
+  );
+};
 
 export default Search;
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import './index.css'
 import App from './App.tsx'
 import {PrimeReactProvider} from "primereact/api";
 import "primereact/resources/themes/arya-blue/theme.css";
+import 'primeicons/primeicons.css';
 
 createRoot(document.getElementById('root')!).render(
   <PrimeReactProvider>
