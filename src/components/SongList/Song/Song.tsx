a5c6f8def6003579da8f04a2e43919a30d44a3c6
Javatlacati <scherzo_16@hotmail.com>
2024-11-19 00:29:06 -0600

    prueba de concepto de mostrar acordes de guitarra o de piano para cada canción

--- src/components/SongList/Song/Song.tsx
+++ src/components/SongList/Song/Song.tsx
@@ -1,10 +1,15 @@
-import {FC, useState} from 'react';
+import {FC, useRef, useState} from 'react';
 import {SongWrapper} from './Song.styled';
 import {Lyric} from "../model/Lyric.ts";
 import {Card} from "primereact/card";
-import {Chord, Interval} from "@tonaljs/tonal";
+import {Chord as KChord, Interval} from "@tonaljs/tonal";
 import {InputNumber, InputNumberValueChangeEvent} from "primereact/inputnumber";
 import ReactPlayer from "react-player";
+import PianoKeyboard from "../PianoKeyboard/PianoKeyboard.lazy.tsx";
+import {Dropdown} from "primereact/dropdown";
+import {Button} from "primereact/button";
+import {OverlayPanel} from "primereact/overlaypanel";
+import Chord from '@tombatossals/react-chords/lib/Chord';
 
 export interface SongProps {
   song: Lyric
@@ -13,6 +18,69 @@ export interface SongProps {
 const Song: FC<SongProps> = ({song}) => {
 
     const [semitones, setSemitones] = useState(0);
+    const [instrumentName, setInstrumentName] = useState(null);
+    const op = useRef(null);
+
+    const [activeNotes, setActiveNotes] = useState({});
+    const instruments = [
+      {name: 'Piano', value: 'piano'},
+      {name: 'Guitar', value: 'guitar'},
+    ]
+
+    const myChord = {
+      frets: [1, 3, 3, 2, 1, 1],
+      fingers: [1, 3, 4, 2, 1, 1],
+      barres: [1],
+      capo: false,
+    }
+    const instrument = {
+      strings: 6,
+      fretsOnChord: 4,
+      name: 'Guitar',
+      keys: [],
+      tunings: {
+        standard: ['E', 'A', 'D', 'G', 'B', 'E']
+      }
+    }
+
+    function mapNotesToActive(transposed: string) {
+      return KChord.get(transposed).notes.reduce((accumulator, currentValue) => {
+        console.log(currentValue);
+        switch (currentValue) {
+          case 'C':
+            return {...accumulator, c: true};
+          case 'C#':
+          case 'Db':
+            return {...accumulator, cs: true};
+          case 'D':
+            return {...accumulator, d: true};
+          case 'D#':
+          case 'Eb':
+            return {...accumulator, ds: true};
+          case 'E':
+            return {...accumulator, e: true};
+          case 'F':
+            return {...accumulator, f: true};
+          case 'F#':
+          case 'Gb':
+            return {...accumulator, fs: true};
+          case 'G':
+            return {...accumulator, g: true};
+          case 'G#':
+          case 'Ab':
+            return {...accumulator, fs: true};
+          case 'A':
+            return {...accumulator, a: true};
+          case 'A#':
+          case 'Bb':
+            return {...accumulator, as: true};
+          case 'B':
+            return {...accumulator, b: true};
+          default:
+            return accumulator;
+        }
+      }, {});
+    }
 
     return (
       <SongWrapper>
@@ -27,6 +95,10 @@ const Song: FC<SongProps> = ({song}) => {
                                     showButtons buttonLayout="horizontal" step={1} min={-11} max={11}
                                     decrementButtonClassName="p-button-danger" incrementButtonClassName="p-button-success"
                                     incrementButtonIcon="pi pi-plus" decrementButtonIcon="pi pi-minus"/>
+            <Dropdown value={instrumentName} onChange={(e) => setInstrumentName(e.value)} options={instruments}
+                      optionLabel="name"
+                      className="w-full md:w-14rem"/>
+            <br/>
           </Card>
           <br/>
           <br/>
@@ -38,11 +110,25 @@ const Song: FC<SongProps> = ({song}) => {
                     paragraph?.chords?.map((chord) => (
                         <>
                           {
-                            chord.lines?.map((line, lineIndex) =>
-                              (
+                            chord.lines?.map((line, lineIndex) => {
+                              const transposed = KChord.transpose(line.chordName, Interval.fromSemitones(semitones));
+                              return (
                                 <span key={'line_' + lineIndex}>
-                                {' '.repeat(line.spacesBefore)}{Chord.transpose(line.chordName, Interval.fromSemitones(semitones))}</span>
-                              ))
+                                {'.'.repeat(line.spacesBefore)}<Button text
+                                                                       onClick={(e) => {
+                                                                         if ('piano' === instrumentName)
+                                                                           setActiveNotes(mapNotesToActive(transposed))
+                                                                         if (instrumentName !== null)
+                                                                           op.current?.toggle(e);
+                                                                       }}>{transposed}</Button>
+                                  <OverlayPanel ref={op}>
+                                    {'guitar' === instrumentName &&
+                                        <Chord instrument={instrument} chord={myChord} lite={false}/>}
+                                    {'piano' === instrumentName && <PianoKeyboard
+                                        activeNotes={activeNotes}/>}
+                                  </OverlayPanel></span>
+                              );
+                            })
                           }
                         </>
                       )
