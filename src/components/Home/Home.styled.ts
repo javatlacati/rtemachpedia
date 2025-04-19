1b5d0bcf1dcaba157ade5bc86887066356a237e7
Javatlacati <scherzo_16@hotmail.com>
2025-04-19 05:26:59 -0600

    des-estructuración de props para quitar warnings

--- src/components/Home/Home.styled.ts
+++ src/components/Home/Home.styled.ts
@@ -1,9 +1,16 @@
 import tw, {styled} from 'twin.macro'
 
-const basecolor = {blue: [tw`bg-blue-900 bg-opacity-50 transform scale-105`, tw`hover:bg-blue-900 hover:bg-opacity-30`],
-yellow:[tw`bg-yellow-900 bg-opacity-50 transform scale-105` , tw`hover:bg-yellow-900 hover:bg-opacity-30`]}
+const colorConfigs = {
+  blue: [tw`bg-blue-900 bg-opacity-50 transform scale-105`, tw`hover:bg-blue-900 hover:bg-opacity-30`],
+  yellow: [tw`bg-yellow-900 bg-opacity-50 transform scale-105`, tw`hover:bg-yellow-900 hover:bg-opacity-30`]
+}
 
-export const MissionWrapper = styled.div`
+interface MissionWrapperProps {
+  active: boolean;
+  basecolor: 'blue' | 'yellow';
+}
+
+export const MissionWrapper = styled.div<MissionWrapperProps>`
     ${tw`p-3 rounded-lg transition-all duration-300`}
-    ${(props) => props.active ? basecolor[props.basecolor][0] : basecolor[props.basecolor][1]}
+    ${({active, basecolor}) => active ? colorConfigs[basecolor][0] : colorConfigs[basecolor][1]}
 `;
